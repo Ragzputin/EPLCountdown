7a388f1b4615cbfeeced52d2f44bf5a79dbc7402
Ragzputin <ray.kumar91@gmail.com>
2015-03-17 12:23:21 -0400

    Made new function called HttpResponseParsing + some major team name lcd printing changes

--- EPLCountdown.ino
+++ EPLCountdown.ino
@@ -33,9 +33,18 @@ int flag2 = 0;
 int flag3 = 0;
 int flag4 = 0;
 int flag5 = 0;
+int homeTeamflag = 0;
+int awayTeamflag = 0;
+int not_EPL = 0;
 char hmTeam[25];
 char awTeam[25];
 char dateTime[25];
+//initialize pointers to the start of homeTeam and awayTeam strings
+//and the lengths of these two strings respectively
+char * tmNameh = &hmTeam[0];
+char * tmNamea = &awTeam[0];
+int lenh = sizeof(hmTeam)/sizeof(hmTeam[0]);
+int lena = sizeof(awTeam)/sizeof(awTeam[0]);  
 
 WiFlyClient client("api.football-data.org", 80);
 
@@ -54,11 +63,11 @@ void setup(){
   Serial.begin(9600);
   Serial.println("Serial begun :D");
   
-  current_time = WiFly.getTime();
+  
   Serial.print("connecting to server...");
   if(client.connect()){
     Serial.println("connected");
-    client.print("GET http://api.football-data.org/teams/563/fixtures/?timeFrame=n8"); //"GET http://api.football-data.org/alpha/soccerseasons/354/leagueTable");
+    client.print("GET http://api.football-data.org/teams/61/fixtures/?timeFrame=n8"); //"GET http://api.football-data.org/alpha/soccerseasons/354/leagueTable");
     client.println(" HTTP/1.1");
     client.println("Host: api.football-data.org");
     client.println("X-Auth-Token: 4f02cc524412487989ee61aed27503d5"); 
@@ -78,49 +87,13 @@ void loop(){
     
     if(flag3 == 1 && letterCount < 320){
       recordMessage(c);
-      if(letterCount == 320){
+      //if(letterCount == 320){
         flag4 = 1;
-      }
+      //}
     }
     
     if(c == '\n' && !client.available() && flag4 == 1){
-      
-      char * ptr2; //declare a second pointer to change reference from start of msg string to start of "homeTeam" - i.e. "h"
-      int count = 0; //this is to count for hmTeam character array
-      int i;
-      for(i = 0; i < 321; i++){
-        if(*(ptr+i) == 'd' && *(ptr+i+1) == 'a' && *(ptr+i+2) == 't' && *(ptr+i+3) == 'e'){
-            ptr2 = ptr+i+7;
-            while(*ptr2 != '"'){
-              dateTime[count] = *ptr2;
-              ptr2++;
-              count++;  
-            } 
-        }
-        count = 0;
-        if(*(ptr+i) == 'h' && *(ptr+i+4) == 'T' && *(ptr+i+7) == 'm'){ //once the sequence "h T m" is found, we know we have reached the word "homeTeam" in the string
-          ptr2 = ptr+i+11;
-          while(*ptr2 != '"'){
-            hmTeam[count] = *ptr2;
-            ptr2++;
-            count++;  
-          } 
-        }
-        count = 0;
-        if(*(ptr+i) == 'a' && *(ptr+i+3) == 'y' && *(ptr+i+4) == 'T' && *(ptr+i+7) == 'm'){
-            ptr2 = ptr+i+11;
-            while(*ptr2 != '"'){
-              awTeam[count] = *ptr2;
-              ptr2++;
-              count++;  
-            } 
-        }
-
-      }
-      Serial.println(dateTime); 
-      Serial.println(hmTeam);
-      Serial.println(awTeam);
-           
+      HttpResponseParsing();
     }
     
     if(c == '{'){
@@ -155,34 +128,16 @@ void loop(){
   }
   
   if(flag5 == 1 && !client.available()){
+    current_time = WiFly.getTime();
     computeTimes();
     countdownFlag = 1;
     flag5 = 0;
   }
   
-  if(countdownFlag == 1){
-    //initialize pointers to the start of homeTeam and awayTeam strings
-    //and the lengths of these two strings respectively
-    char * tmNameh = &hmTeam[0];
-    char * tmNamea = &awTeam[0];
-    int lenh = sizeof(hmTeam)/sizeof(hmTeam[0]);
-    int lena = sizeof(awTeam)/sizeof(awTeam[0]);
-    
-
-    lcd.clear();
-    lcd.setCursor(0,0);
-    teamNamePrint(tmNameh, lenh);
-    lcd.print(" v ");
-    teamNamePrint(tmNamea, lena);
+  if(countdownFlag == 1 && not_EPL != 1){
+    lcd.setCursor(0,1);
     lcd.setBacklight(LOW);
-    if(timediff > 0){
-      countdown();
-    } else {
-      lcd.setCursor(0,1);
-      lcd.print("Game Done!");
-      countdownFlag = 0;
-    }
-
+    countdown();
   }
   
 }
@@ -193,58 +148,136 @@ void recordMessage(char message){
   delay(1);
 }
 
+void HttpResponseParsing(){
+
+  char * ptr2; //declare a second pointer to change reference from start of msg string to start of date i.e. "d"
+  int count = 0; //this is to count for hmTeam character array
+  int i;
+  for(i = 0; i < 321; i++){
+    if(*(ptr+i) == 'd' && *(ptr+i+1) == 'a' && *(ptr+i+2) == 't' && *(ptr+i+3) == 'e'){
+        ptr2 = ptr+i+7;
+        while(*ptr2 != '"'){
+          dateTime[count] = *ptr2;
+          ptr2++;
+          count++;  
+        } 
+    }
+    count = 0;
+    if(*(ptr+i) == 'h' && *(ptr+i+4) == 'T' && *(ptr+i+7) == 'm'){ //once the sequence "h T m" is found, we know we have reached the word "homeTeam" in the string
+        ptr2 = ptr+i+11;
+        while(*ptr2 != '"'){
+          hmTeam[count] = *ptr2;
+          ptr2++;
+          count++;  
+        }
+        lcd.clear();
+        lcd.setCursor(0,0);
+        homeTeamflag = 1;
+        teamNamePrint(tmNameh, lenh);
+        homeTeamflag = 0; 
+    }
+    count = 0;
+    if(*(ptr+i) == 'a' && *(ptr+i+3) == 'y' && *(ptr+i+4) == 'T' && *(ptr+i+7) == 'm'){
+        ptr2 = ptr+i+11;
+        while(*ptr2 != '"'){
+          awTeam[count] = *ptr2;
+          ptr2++;
+          count++;  
+        }
+        if(not_EPL != 1){
+          awayTeamflag = 1; 
+          teamNamePrint(tmNamea, lenh);
+          awayTeamflag = 0;
+        }
+    }
+
+  }
+  Serial.println(dateTime); 
+  Serial.println(hmTeam);
+  Serial.println(awTeam);
+}
 
 void teamNamePrint(char * teamName, int length){
   int i;
+  
   for(i = 0; i < length; i++){
     //Here we look up the team's full name given in the HTTP response and print out the short name to the LCD screen
-    if(*(teamName + i) == 'F' && *(teamName + i + 1) == 'C' && *(teamName + i + 3) == 'A' && *(teamName + i + 4) == 'r')
+    if(*(teamName + i) == 'F' && *(teamName + i + 1) == 'C' && *(teamName + i + 3) == 'A' && *(teamName + i + 4) == 'r'){
       lcd.print("ARS");
-    else if(*(teamName + i) == 'A' && *(teamName + i + 1) == 's' && *(teamName + i + 2) == 't')
+      break;
+    }else if(*(teamName + i) == 'A' && *(teamName + i + 1) == 's' && *(teamName + i + 2) == 't'){
       lcd.print("AVFC");
-    else if(*(teamName + i) == 'B' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'r')
+      break;
+    }else if(*(teamName + i) == 'B' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'r'){
       lcd.print("BUR");
-    else if(*(teamName + i) == 'C' && *(teamName + i + 1) == 'r' && *(teamName + i + 2) == 'y')
+      break;
+    }else if(*(teamName + i) == 'C' && *(teamName + i + 1) == 'r' && *(teamName + i + 2) == 'y'){
       lcd.print("CRP");
-    else if(*(teamName + i) == 'C' && *(teamName + i + 1) == 'h' && *(teamName + i + 2) == 'e')
+      break;
+    }else if(*(teamName + i) == 'C' && *(teamName + i + 1) == 'h' && *(teamName + i + 2) == 'e'){
       lcd.print("CHE");
-    else if(*(teamName + i) == 'E' && *(teamName + i + 1) == 'v' && *(teamName + i + 2) == 'e')
+      break;
+    }else if(*(teamName + i) == 'E' && *(teamName + i + 1) == 'v' && *(teamName + i + 2) == 'e'){
       lcd.print("EVR");
-    else if(*(teamName + i) == 'H' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'l')
+      break;
+    }else if(*(teamName + i) == 'H' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'l'){
       lcd.print("HUL");
-    else if(*(teamName + i) == 'L' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 'i')
+      break;
+    }else if(*(teamName + i) == 'L' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 'i'){
       lcd.print("LEIC");
-    else if(*(teamName + i) == 'L' && *(teamName + i + 1) == 'i' && *(teamName + i + 2) == 'v')
+      break;
+    }else if(*(teamName + i) == 'L' && *(teamName + i + 1) == 'i' && *(teamName + i + 2) == 'v'){
       lcd.print("LIV");
-    else if(*(teamName + i) == 'M' && *(teamName + i + 1) == 'a' && *(teamName + i + 2) == 'n' && *(teamName + i + 11) == 'C')
+      break;
+    }else if(*(teamName + i) == 'M' && *(teamName + i + 1) == 'a' && *(teamName + i + 2) == 'n' && *(teamName + i + 11) == 'C'){
       lcd.print("MCITY");
-    else if(*(teamName + i) == 'M' && *(teamName + i + 1) == 'a' && *(teamName + i + 2) == 'n' && *(teamName + i + 11) == 'U')
+      break;
+    }else if(*(teamName + i) == 'M' && *(teamName + i + 1) == 'a' && *(teamName + i + 2) == 'n' && *(teamName + i + 11) == 'U'){
       lcd.print("MANU");
-    else if(*(teamName + i) == 'N' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 'w')
+      break;
+    }else if(*(teamName + i) == 'N' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 'w'){
       lcd.print("NEW");
-    else if(*(teamName + i) == 'Q' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'e')
+      break;
+    }else if(*(teamName + i) == 'Q' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'e'){
       lcd.print("QPR");
-    else if (*(teamName + i) == 'F' && *(teamName + i + 1) == 'C' && *(teamName + i + 3) == 'S' && *(teamName + i + 4) == 'o')
+      break;
+    }else if (*(teamName + i) == 'F' && *(teamName + i + 1) == 'C' && *(teamName + i + 3) == 'S' && *(teamName + i + 4) == 'o'){
       lcd.print("SOU");
-    else if(*(teamName + i) == 'S' && *(teamName + i + 1) == 't' && *(teamName + i + 2) == 'o')
+      break;
+    }else if(*(teamName + i) == 'S' && *(teamName + i + 1) == 't' && *(teamName + i + 2) == 'o'){
       lcd.print("STK");
-    else if(*(teamName + i) == 'S' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'n')
+      break;
+    }else if(*(teamName + i) == 'S' && *(teamName + i + 1) == 'u' && *(teamName + i + 2) == 'n'){
       lcd.print("SUND");
-    else if(*(teamName + i) == 'S' && *(teamName + i + 1) == 'w' && *(teamName + i + 2) == 'a')
+      break;
+    }else if(*(teamName + i) == 'S' && *(teamName + i + 1) == 'w' && *(teamName + i + 2) == 'a'){
       lcd.print("SWA");
-    else if(*(teamName + i) == 'T' && *(teamName + i + 1) == 'o' && *(teamName + i + 2) == 't')
+      break;
+    }else if(*(teamName + i) == 'T' && *(teamName + i + 1) == 'o' && *(teamName + i + 2) == 't'){
       lcd.print("SPURS");
-    else if(*(teamName + i) == 'W' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 's' && *(teamName + i + 5) == 'B')
+      break;
+    }else if(*(teamName + i) == 'W' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 's' && *(teamName + i + 5) == 'B'){
       lcd.print("WBA");
-    else if(*(teamName + i) == 'W' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 's' && *(teamName + i + 5) == 'H')
+      break;
+    }else if(*(teamName + i) == 'W' && *(teamName + i + 1) == 'e' && *(teamName + i + 2) == 's' && *(teamName + i + 5) == 'H'){
       lcd.print("WHU");
-    
+      break;
+    } else{
+      lcd.print("No EPL games");
+      lcd.setBacklight(LOW);
+      not_EPL = 1;
+      break;
+    }
+  }
+  
+  if(not_EPL != 1 && homeTeamflag == 1){
+    lcd.print(" v ");
   }
 }
 
 
 void computeTimes(){
-    if(lpcount < 60 && timediff > 0){
+    if(lpcount < 60){
       gametime_calc();
       timediff = gametime - current_time;
       days = timediff / 86400;
@@ -253,7 +286,7 @@ void computeTimes(){
       rem2 = rem1 % 3600;
       mins = rem2 / 60;
       sec = rem2 % 60;
-    } else if(lpcount == 60 && timediff > 0){
+    } else if(lpcount == 60){
       lpcount = 0;
       current_time = WiFly.getTime();
       new_timediff = gametime - current_time;
@@ -308,7 +341,7 @@ void countdown(){
   }
   
   if(lpcount == 60){
-    checkAction();
+    computeTimes();
     lcd.setCursor(0,1);
     lcd_print(days,0); //print days
     lcd_print(hrs,3); //print hours
@@ -348,4 +381,5 @@ void gametime_calc(){
   tm.Year = ((dateTime[0] - '0')*1000 + (dateTime[1] - '0')*100 + (dateTime[2] - '0')*10 + (dateTime[3] - '0')) - 1970;
   
   gametime = makeTime(tm); //game time as time_t variable
+  
 }
